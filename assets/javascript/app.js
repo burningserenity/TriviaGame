a910fc9b9e5c4b172573e7b417b03d3ea7b41812
burningserenity <burningserenity@bitmai.la>
2017-07-02 16:09:39 -0400

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+/*Questions and Answers
+
+What is your name?
+
+What is your favorite color?
+
+What is the airspeed velocity of an unladen swallow? The airspeed velocity of an unladen swallow is roughly 11 meters per second, or 24 miles per hour, beating its wings 7-9 times per second rather than 43. It's true: a 5 ounce bird cannot carry a one pound coconut, but furthermore, no swallow weighs 5 ounces. The barn swallow, which is what most English people mean when they say "swallow", weighs only 20 grams (2/3 of an ounce). ...?
+
+We are no longer the Knights Who Say 'Ni,' we are the Knights who say:
+
+When throwing the Holy Handgrenade, what shall be the number of counting?
+
+The Grail is located in:
+
+What are the names of the Knights of the Round Table?
+
+According to the French, your mother is a _ ?
+
+And your father smells of _ ?
+
+Which seminal Rock album helped fund the production? Dark Side of the Moon
+
+The picture of God is actually a picture of whom? 19th Century English cricketer W.G Grace
+
+Why do witches burn? Because they're made of wood
+*/
